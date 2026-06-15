e77f8800f3b037c512a5ba1f0debf28db1a97ff0
Subhadip Bag <subhadipbag906@gmail.com>
2026-06-15 21:05:51 +0530

    exit on no wallpaper and set using gsettings

--- main.sh
+++ main.sh
@@ -8,5 +8,19 @@ while true; do
     # Get all images
     mapfile -t wallpapers < < (
         find "$WALLPAPER_DIR" -type f \
-            \( -iname "*.jpg" -o iname )
+            \( -iname "*.jpg" -o iname "*.jpge" -o -iname "*.png" -o -iname "*.webp" \)
     )
+
+    # Exit if no wallpaper found
+    [ ${#wallpapers[@]} -eq 0] && exit 1
+
+    # Select a random wallpaper
+    wallpaper="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
+
+    echo "Setting: $wallpaper"
+
+    gsettings set org.gnome.desktop.background picture-uri "file://$wallpaper"
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$wallpaper"
+
+    sleep "$INTERVAL"
+done
